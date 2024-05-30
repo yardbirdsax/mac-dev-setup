8c1664a66b221b949dcdedd68563ae5ed49c884e
Josh Feierman <josh@sqljosh.com>
2024-05-29 20:28:55 -0400

    chore: fix for ansible lint issues

--- ansible/run.sh
+++ ansible/run.sh
@@ -14,5 +14,4 @@ echo "Installing Ansible" && brew install ansible && \
 echo "Installing Ansible collections" && ansible-galaxy collection install -r requirements.yaml && \
 echo "Installing Ansible roles" && ansible-galaxy install -r requirements.yaml -p vendor --force && \
 echo "Running Ansible playbook" && ansible-playbook -K main.yaml && \
-echo "Setting Git to use OSX keychain for credentials" && git config --global credential.helper osxkeychain && \
-echo "Installing VS Code plugins" && ./install-vscode-plugins.sh
+echo "Setting Git to use OSX keychain for credentials" && git config --global credential.helper osxkeychain
