6e50e66f289b8268af2dc2fb132fff10b663b172
Josh Feierman <josh@sqljosh.com>
2025-07-08 11:52:20 -0400

    various fixes

--- ansible/run.sh
+++ ansible/run.sh
@@ -3,15 +3,25 @@
 echo "" > install.log
 
 # Install Homebrew
-echo "Installing Homebrew" && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# echo "Installing Homebrew" && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "Installing xcode tools" && xcode-select --install
+# echo "Installing xcode tools" && xcode-select --install
 
-# Install ansible
-echo "Installing Ansible" && brew install ansible && \
+export PATH=$HOME/.asdf/shims:$(/opt/homebrew/bin/brew --prefix)/bin:$PATH
+export ASDF_PYAPP_INCLUDE_DEPS=1
+
+# Install dependencies for Python
+echo "Installing Python dependencies" && \
+brew install openssl readline sqlite3 xz zlib tcl-tk@8 libb2
+
+# Install asdf
+echo "Installing ASDF" && brew install asdf && \
+echo "Installing Python plugin for ASDF" && asdf plugin add python && \
+asdf install && \
+echo "Installing Ansible" && pip install ansible && \
 
 # Install geerlingguy role
 echo "Installing Ansible collections" && ansible-galaxy collection install -r requirements.yaml && \
-echo "Installing Ansible roles" && ansible-galaxy install -r requirements.yaml -p vendor --force && \
+echo "Installing Ansible roles" && ansible-galaxy install -r requirements.yaml -p vendor --force
 echo "Running Ansible playbook" && ansible-playbook -K main.yaml && \
 echo "Setting Git to use OSX keychain for credentials" && git config --global credential.helper osxkeychain
